e6112c8cf3e9c7f036b35d44dc5b6c14c9b268f4
RedHerrings <lin800109@gmail.com>
2014-07-27 02:43:45 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,11 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+## I use cacheSolve to actually solve the inverse matrices or just retrive from cache.
+## If the inverse matrix is already solved and saved in cache, it will be returned directly from cache;
+## if not, cacheSolve will solve the inverse matrices and save it in cache, 
+## and could be retrieved next time when I call cachesolve.
+
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
